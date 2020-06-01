279d6a86c9954a882161b6920ecb74cd6c5a41be
GoDK36 <siinwoo0306@gmail.com>
2020-06-01 23:53:13 +0900

    학교 과제

--- 언프1/assignment7.py
+++ 언프1/assignment7.py
@@ -1,16 +1,14 @@
-## alice.txt 파일을 열고 읽어서
-##
-## -기호 모두 삭제
-##
-## -모두 소문자로 바꾸기
-##
-## -각 단어의 빈도 세고, 딕셔너리 형태로 저장하기(예: “apple”: 3)
+## alice.txt 파일을 열고 읽기
 
 file = open(r"E:\Develop\python\school-project\언프1\alice.txt", "r")
 alice = file.read()
 
 # 소문자화
 lower_alice = alice.lower()
+print("\n##################")
+print("#### 소문자화 ####")
+print("##################\n")
+print(lower_alice)
 
 #기호 모두 삭제
 del_punc_alice = lower_alice.replace(",", '')
@@ -18,6 +16,11 @@ del_punc_alice = del_punc_alice.replace('\'', '')
 del_punc_alice = del_punc_alice.replace('.', '')
 del_punc_alice = del_punc_alice.replace('"', '')
 wrd_alice = del_punc_alice.split()
+print("\n########################")
+print("#### 기호 모두 삭제 ####")
+print("########################\n")
+print(wrd_alice)
+
 
 # unique word 만들기
 unique_word_list = list(set(wrd_alice))
@@ -28,25 +31,46 @@ alice_dict = {}
 for word in unique_word_list:
     alice_dict[word] = wrd_alice.count(word)
 
-# 알파벳 순으로 단어 출력
+
+print("\n##########################")
+print("#### 빈도 수 딕셔너리 ####")
+print("##########################\n")
+print(alice_dict)
+
+# alice_word_list.txt에 저장
+
+file2 = open("alice_word_list.txt", "w")
 
 print("\n#################################")
 print("#### 알파벳 순으로 단어 출력 ####")
 print("#################################\n")
-print(unique_word_list)
 
-# unique 단어 수 출력
+for word in unique_word_list:
+    # 화면에 출력
+    print(word)
+    file2.write(word + '\n')
 
 print("\n#############################")
 print("#### unique 단어 수 출력 ####")
 print("#############################\n")
 print("The number of words in the data is " + str(len(unique_word_list)))
 
-# alice_word_list.txt에 저장
+file2.write("The number of words in the data is " + str(len(unique_word_list)))
+file2.close()
 
-file2 = open("alice_word_list.txt", "w")
-file2.write(unique_word_list, "'\n'The number of words in the data is " + str(len(unique_word_list)))
+# "단어: 빈도"로 되있는 딕셔너리 줄바꿈해서 출력 및 저장
 
-# "단어: 빈도"로 되있는 딕셔너리 줄바꿈해서 출력
+file3 = open("alice_freq.txt", "w")
 
-# alice_freq.txt 에 저장
+print("\n###########################")
+print("#### 단어: 빈도로 출력 ####")
+print("###########################\n")
+
+for word, freq in alice_dict.items():
+    # 화면에 출력
+    print(word + ': ' + str(freq))
+    
+    # alice_freq.txt 에 저장
+    file3.write(word + ': ' + str(freq) + '\n')
+
+file3.close()
