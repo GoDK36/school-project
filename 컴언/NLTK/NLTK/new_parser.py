71ec6393de92dba05ef0adaa14b3bbc256508a7c
GoDK36 <siinwoo0306@gmail.com>
2020-06-14 18:46:49 +0900

    NLTK CFG 과제

---
+++ 컴언/NLTK/NLTK/new_parser.py
@@ -0,0 +1,45 @@
+################################################################
+################ Just Change this sections #####################
+################################################################
+# (1) Type Your Sentence
+
+sents = ["The company hired a man with sincerity.",
+        "Which option do you want to select?",
+        "The shop manager wrote an email to the customer.",
+        "While you were taking a nap, I was working overtime.",
+        "The professor permitted the students to take a nap during the class."]
+
+# (2) Make your CFG file for parsing your sentence.
+
+# ex. grammar.cfg (see the direction in the powerpoint)
+
+################################################################
+
+import nltk
+grammar = nltk.data.load(r"file:E:\Develop\python\school-project\컴언\NLTK\NLTK\new_grammar.cfg")
+parser = nltk.ChartParser(grammar)
+
+n = 1
+for sent in sents:
+    tokens = nltk.tokenize.word_tokenize(sent)
+    trees = parser.parse(tokens)
+
+    with open(r"E:\Develop\python\school-project\컴언\NLTK\NLTK\result" + str(n) + ".txt", "w") as f1:
+        for tree in trees:
+            print(tree)
+            f1.write(str(tree)+"\n\n")
+            tree.draw()
+    f1.close()
+    
+    n += 1
+
+"""
+단어를 정확히 입력??(구두점까지?)
+너무 많이 가지수가 나옴
+
+
+참고 사이트
+http://erg.delph-in.net/logon
+https://www.link.cs.cmu.edu/cgi-bin/link/construct-page-4.cgi#submit
+
+"""
--- 컴언/NLTK/NLTK/parser.py
+++ 컴언/NLTK/NLTK/parser.py
@@ -3,8 +3,7 @@
 ################################################################
 # (1) Type Your Sentence
 
-sent = "I saw the man with a telescope"
-
+sent = "Which option do you want to select?"
 # (2) Make your CFG file for parsing your sentence.
 
 # ex. grammar.cfg (see the direction in the powerpoint)
@@ -12,13 +11,14 @@ sent = "I saw the man with a telescope"
 ################################################################
 
 import nltk
-grammar = nltk.data.load(r"file:E:\Develop\python\school-project\컴언\NLTK\NLTK\grammar.cfg")
+grammar = nltk.data.load(r"file:E:\Develop\python\school-project\컴언\NLTK\NLTK\grammar3.cfg")
 parser = nltk.ChartParser(grammar)
 tokens = nltk.tokenize.word_tokenize(sent)
 trees = parser.parse(tokens)
 
-with open(r"E:\Develop\python\school-project\컴언\NLTK\NLTK\result.txt", "w") as f1:
+with open(r"E:\Develop\python\school-project\컴언\NLTK\NLTK\result3.txt", "w") as f1:
     for tree in trees:
         print(tree)
         f1.write(str(tree)+"\n\n")
         tree.draw()
+
